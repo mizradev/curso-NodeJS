cf7860a86f095c5aea6c47045a7329cd1a64c37e
soymizra <jorgemizrain@gmail.com>
2019-04-07 12:08:52 -0600

    Detalles finales para el proyecto del webserver

--- webserver/server.js
+++ webserver/server.js
@@ -21,6 +21,6 @@ app.get('/about', (req, res) => {
 });
 
 
-app.listen(3000, ()=>{
-  console.log('Server running on: http://localhost:3000');
+app.listen(port, ()=>{
+  console.log('Server running on: http://localhost:'+port);
 });
